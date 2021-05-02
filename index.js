e3fb8610a3b76d0cefacde46ed1e86791a143fd7
Keagan <esorts.k990@gmail.com>
2021-05-02 18:51:40 -0400

    added post request

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const ObjectID = require('mongodb').ObjectID
 
-mongoose.connect('mongodb+srv://data:1234@cluster0.qvhok.mongodb.net/Facebook?retryWrites=true&w=majority', {useUnifiedTopology:true, useNewUrlParser:true})
+mongoose.connect('mongodb+srv://data:1234@cluster0.qvhok.mongodb.net/AlphaOmega?retryWrites=true&w=majority', {useUnifiedTopology:true, useNewUrlParser:true})
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error'))
@@ -16,35 +16,15 @@ db.once('open',function callback () {
     console.log("database is up and running")
 } )
 
-app.post('/users',  async (req,res) => {
-    const newUser = req.body
-    const x = await db.collection('users').insertOne(newUser)
-    res.json("user successfully added to database")
-    console.log(newUser)
+app.post('/requests',  async (req,res) => {
+    const newRequest = req.body
+    try{
+    const x = await db.collection('requests').insertOne(newRequest)
+    res.json("Request successfully sent to database")
+    } catch(error){
+    console.log(newRequest)}
 })
 
-app.get('/users', async(req,res)  => {
-    const getUsers = await db.collection('users').find({}).toArray()
-    res.json(getUsers)
-    console.log(getUsers)
-})
-
-app.get('/users/:name', async(req,res) => {
-    const name = req.params.name
-    const info = await db.collection('users').find({name:name}).toArray()
-    res.json(info);
-})
-app.get('/users/:userId', async(req,res) => {
-    const id = req.params.userId
-    const info = await db.collection('users').find({userId:userId}).toArray()
-    res.json(info)
-})
-
-app.delete('/users/:userId', async(req,res) => {
-    const id = req.params.userId
-    const x = await db.collection('users').deleteOne({_id: new ObjectID (id)})
-    res.json('user deleted')
-})
 
 const port = process.env.PORT || 3001
 app.listen (port, () => console.log(`server is running on port ${port}... `))
